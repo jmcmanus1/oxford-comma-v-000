be2ccd68b3b233fea484878a14170463418315cd
Julie  McManus <julieannesays@gmail.com>
2020-01-23 17:48:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
+  elsif array.length == 3
+    array.join(" , " + " and ")
   elsif array.length > 3
     array.join(" , " + " and ")
+
   else
     array.join(" , ")
   end
