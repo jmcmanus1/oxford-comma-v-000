95c2f09dd387dc86d360685fbf36e8de641d1fb4
Julie  McManus <julieannesays@gmail.com>
2020-01-23 18:37:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
   end
-  array.join(", ")
+  #elsif 2 < array.length
+    #array[-1].insert(0, "and ")
+  #end
+  #array.join(", ")
 end
